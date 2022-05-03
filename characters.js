2b2b2949cda06e578cda24fcdd6f682910632fea
Hieu Ma <97485726+Hieu-Ma1@users.noreply.github.com>
2022-05-02 20:54:45 -0700

    saving latest changes

--- characters.js
+++ characters.js
@@ -1,3 +1,4 @@
+
 let player = {
     health: 100,
 }
@@ -26,21 +27,44 @@ let bosses = [
 let player_health = document.getElementById("player_health");
 let boss_health = document.getElementById("boss_health");
 
+let boss_title = document.getElementById("boss_health");
+boss_title.innerHTML = bosses[bosses.length - 1].name;
+
+let boss_appearance = document.getElementById("boss");
+boss_appearance.innerHTML = `<object id="enemy" data="./images/${bosses[bosses.length - 1].name}.svg" width="500px"  height="500px"> </object>`
+
+
 // User Damage and Healing
 
 let attack = document.getElementById("attack");
 attack.addEventListener('click', () => {
+    // if (boss.health <= 0) {
+    //     bosses.pop();
+    //     let boss_title = document.getElementById("boss_health");
+    //     boss.health = 100;
+    //     boss_health.style.width = String(boss_health);
+    //     boss_title.innerHTML = bosses[bosses.length - 1].name;
+    //     boss_appearance.innerHTML = `<object id="enemy" data="./images/${bosses[bosses.length - 1].name}.svg" width="500px"  height="500px"> </object>`
+    // } else {
+    //     boss.health -= 8;
+    //     boss_health_string = String(boss.health + "%");
+    //     boss_health.style.width = boss_health_string;
+
+    // }
+    boss.health -= 8;
+    boss_health_string = String(boss.health + "%");
+    boss_health.style.width = boss_health_string;
+
     if (boss.health <= 0) {
         bosses.pop();
+        if (bosses.length === 0) {
+            alert('you won');
+            clearInterval(timerId);
+        }
+        boss_title.innerHTML = bosses[bosses.length - 1].name;
         boss.health = 100;
-    } else {
-        boss.health -= 8;
-        boss_health_string = String(boss.health + "%");
-        boss_health.style.width = boss_health_string;
-    }
-    if(bosses.length === 0) {
-        alert('you won');
-        clearInterval(timerId); 
+        boss_health.style.width = "100%";
+        boss_appearance.innerHTML = `<object id="enemy" data="./images/${bosses[bosses.length - 1].name}.svg" width="500px"  height="500px"> </object>`
     }
 })
 
@@ -58,7 +82,6 @@ healing_flask.addEventListener('click', () => {
 
 // Boss Damage Mechanic
 const bossMechanic = () => {
-    console.log("boss damage", bosses[bosses.length - 1].damage)
     player.health -= bosses[bosses.length - 1].damage;
     player_health_string = String(player.health + "%")
     player_health.style.width = player_health_string;
@@ -66,13 +89,14 @@ const bossMechanic = () => {
 }
 
 const didPlayerLose = () => {
-    if(player.health <= 0 ) {
-        clearInterval(timerId); 
-        if(player.health <= 0) {
+    if (player.health <= 0) {
+        clearInterval(timerId);
+        if (player.health <= 0) {
             player_health.style.width = "0%";
             alert('you have lost');
         }
     }
+
 }
 
 let timerId = setInterval(() =>
